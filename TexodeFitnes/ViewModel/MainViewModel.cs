bd319ee6a84e2c554239d77000879c2d17b8c6d0
tuuuwerk@outlook.com <tuuuwerk@outlook.com>
2023-03-29 10:48:51 +0300

    colored points, two graph, fix user bags

--- TexodeFitnes/ViewModel/MainViewModel.cs
+++ TexodeFitnes/ViewModel/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -41,20 +44,20 @@ namespace TexodeFitnes.ViewModel
                     string jsonString = File.ReadAllText(fileName);
                     JsonNode forecastNode = JsonNode.Parse(jsonString)!;
 
-                    for (int j = 0; j < forecastNode.AsArray().Count-1; j++)
+                    for (int j = 0; j < forecastNode.AsArray().Count; j++)
                     {
                         
-                        int RankNode = forecastNode![j]["Rank"]!.GetValue<int>();
-                        string UserNode = forecastNode![j]["User"]!.GetValue<string>();
-                        string StatusNode = forecastNode![j]["Status"]!.GetValue<string>();
-                        int StepsNode = forecastNode![j]["Steps"]!.GetValue<int>();
+                        int rankNode = forecastNode![j]["Rank"]!.GetValue<int>();
+                        string userNode = forecastNode![j]["User"]!.GetValue<string>();
+                        string statusNode = forecastNode![j]["Status"]!.GetValue<string>();
+                        int stepsNode = forecastNode![j]["Steps"]!.GetValue<int>();
 
-                        if (!_users.ContainsKey(UserNode))
+                        if (!_users.ContainsKey(userNode))
                         {
-                            _users.Add(UserNode, new Users());
-                            _users[UserNode].User = UserNode;
+                            _users.Add(userNode, new Users());
+                            _users[userNode].User = userNode;
                         }
-                        _users[UserNode].AddDay(i, RankNode, StatusNode, StepsNode);
+                        _users[userNode].AddDay(i, rankNode, statusNode, stepsNode);
                     }
                 }
             }
@@ -73,26 +76,100 @@ namespace TexodeFitnes.ViewModel
             get { return _messageError; }
         }
 
+        bool _stepsChart;
 
-        //private int _number1;
-        //public int Number1
-        //{
-        //    get { return _number1; }
-        //    set
-        //    {
-        //        _number1 = value;
-        //        OnPropertyChanged("Number3"); // уведомление View о том, что изменилась сумма
-        //    }
-        //}
+        public bool StepsChart
+        {
+            get { return _stepsChart; }
+            set 
+            {
+                _stepsChart = value;
+                OnPropertyChanged("StepsChart");
+            }
+        }
+        public void Chart()
+        {
 
-        //private int _number2;
-        //public int Number2
-        //{
-        //    get { return _number2; }
-        //    set { _number2 = value; OnPropertyChanged("Number3"); }
-        //}
+            this.MyModel = new PlotModel { Title = "Шаги" };
 
-        ////свойство только для чтения, оно считывается View каждый раз, когда обновляется Number1 или Number2
-        //public int Number3 => Number1 + Number2;
+
+            if (_stepsChart)
+            {
+
+                var line1 = new OxyPlot.Series.LineSeries()
+                {
+                    Color = OxyPlot.OxyColors.Blue,
+                    StrokeThickness = 1,
+                    InterpolationAlgorithm = InterpolationAlgorithms.UniformCatmullRomSpline,
+                    MarkerType = MarkerType.Circle,
+                };
+                var upperStepsLine= new OxyPlot.Series.LineSeries()
+                {
+                    Color = OxyColors.Transparent,
+                    MarkerFill = OxyPlot.OxyColors.Green,
+                    MarkerType = MarkerType.Circle,
+                    MarkerSize = 5
+                };
+                var lowerStepsLine = new OxyPlot.Series.LineSeries()
+                {
+                    Color = OxyColors.Transparent,
+                    MarkerFill = OxyPlot.OxyColors.Red,
+                    MarkerType = MarkerType.Circle,
+                    MarkerSize = 5
+                };
+
+                for (int i = 0; i < _selectedUser.Steps.Length; i++)
+                {
+                    line1.Points.Add(new OxyPlot.DataPoint(i + 1, _selectedUser.Steps[i]));
+                    if (_selectedUser.UpperSteps == _selectedUser.Steps[i])  upperStepsLine.Points.Add(new OxyPlot.DataPoint(i + 1, _selectedUser.Steps[i]));;
+                    if (_selectedUser.LowerSteps == _selectedUser.Steps[i])  lowerStepsLine.Points.Add(new OxyPlot.DataPoint(i + 1, _selectedUser.Steps[i]));;
+                }
+                
+                this.MyModel.Series.Add(line1);
+                this.MyModel.Series.Add(upperStepsLine);
+                this.MyModel.Series.Add(lowerStepsLine);
+
+            }
+            else
+            {
+                this.MyModel.Axes.Add(new LinearAxis());
+                double x = 0.0;
+                for (int i = 0; i < _selectedUser.Steps.Length; i++)
+                {
+                    var histogramSeries = new HistogramSeries()
+                    {
+                        StrokeThickness = 1,
+                        FillColor = OxyColors.LightBlue,
+                        StrokeColor = OxyColors.LightCyan
+                    };
+                    if (_selectedUser.UpperSteps == _selectedUser.Steps[i]) histogramSeries.FillColor = OxyColors.LightGreen;
+                    if (_selectedUser.LowerSteps == _selectedUser.Steps[i]) histogramSeries.FillColor = OxyColors.Orange;
+                    histogramSeries.Items.Add(new HistogramItem(x + 0.0, x + 1, _selectedUser.Steps[i], 1));
+                    MyModel.Series.Add(histogramSeries);
+
+                    x += 1;
+                }
+            }
+            OnPropertyChanged("MyModel");
+        }
+        public PlotModel MyModel { get; private set; }
+
+
+        private Users _selectedUser;
+        public Users SelectedUser
+        {
+            get
+            {
+                return this._selectedUser;
+            }
+
+            set
+            {
+                this._selectedUser = value;
+                OnPropertyChanged("SelectedUser");
+                Chart();
+            }
+        }
+
     }
 }
